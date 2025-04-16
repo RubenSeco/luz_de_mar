b1701195fab999676a64a07fa7d70b0ad927dad0
Rubén Seco <ruben.seco.rs@gmail.com>
2025-04-16 15:34:42 -0300

    actualizacion 16/04/25

--- src/app/ui/Header.tsx
+++ src/app/ui/Header.tsx
@@ -29,44 +29,39 @@ export const Categories = [
 export const Header = () => {
   return (
     <>
-      <header className='bg-white w-full border-b-2 border-b-gray-200'>
-        <div className='mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8'>
-          <div className='flex h-16 items-center justify-between'>
-            <div className='md:flex md:items-center md:gap-12'>
-              <Link href='/dashboard'>
-                <Image
-                  src='/images/LOGOdibujo.jpg'
-                  style={{
-                    height: 'auto',
-                    width: 'auto'
-                  }}
-                  alt='Logo'
-                  width={50}
-                  height={50}
-                />
-              </Link>
-            </div>
-
-            <div className='hidden md:block'>
-              <nav aria-label='Global'>
-                <ul className='flex items-center gap-6 text-base'>
-                  {Object.values(Categories).map((category) => (
-                    <li key={category.name}>
-                      <Link
-                        className='text-teal-700 transition-all font-montserrat font-bold hover:text-teal-600/80'
-                        href={`/dashboard/${category.name}`}>
-                        {category.title}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </nav>
-            </div>
-
-            <div className='flex items-center gap-4 text-black z-30 bg-teal-600'></div>
-            <DropDownMenu />
+      <header className='bg-white border-b-2 border-b-gray-200 px-4 sm:px-6 flex h-16 items-center justify-between'>
+        <div className='flex items-center gap-4'>
+          <Link href='/dashboard'>
+            <Image
+              src='/images/LOGOdibujo.jpg'
+              style={{
+                height: 'auto',
+                width: 'auto'
+              }}
+              alt='Logo'
+              width={50}
+              height={50}
+            />
+          </Link>
+          <div className='hidden md:flex'>
+            <nav aria-label='Global'>
+              <ul className='flex items-center gap-6 text-base'>
+                {Object.values(Categories).map((category) => (
+                  <li key={category.name}>
+                    <Link
+                      className='text-teal-700 transition-all font-montserrat font-bold hover:text-teal-600/80'
+                      href={`/dashboard/${category.name}`}>
+                      {category.title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </nav>
           </div>
         </div>
+
+        <div className='flex items-center gap-4 text-black z-30 bg-teal-600'></div>
+        <DropDownMenu />
       </header>
     </>
   );
